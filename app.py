104fb7fdfe896be163f07282ea2e2d96ccf3fd4f
AndyCorRc <andycornejo2002@gmail.com>
2024-12-13 14:03:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -252,11 +252,11 @@ def fetch_team_details(url):
 
     # Extract specific team details based on the page structure
     details = {
-        'nombre': safe_get_text(soup.find('strong')),  # Team name
-        'nombreCompleto': safe_get_text(soup.find(text='Nombre completo:').find_next('br').next_sibling.strip()),
-        'fundado': safe_get_text(soup.find(text='Fundación:').find_next('br').next_sibling.strip()),
-        'apodo': safe_get_text(soup.find(text='Apodo:').find_next('br').next_sibling.strip()),
-        'estadio': safe_get_text(soup.find(text='Estadio local:').find_next('br').next_sibling.strip()),
+        'name': safe_get_text(soup.find('strong')),  # Team name
+        'nombreCompleto': safe_get_text(soup.find(text='Nombre completo:').find_next('/i')),
+        'fundado': safe_get_text(soup.find(text='Fundación:').find_next('/i')),
+        'apodo': safe_get_text(soup.find(text='Apodo:').find_next('i')),
+        'estadio': safe_get_text(soup.find(text='Estadio local:').find_next('br')),
     }
     return details
 
